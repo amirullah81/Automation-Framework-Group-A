b687d7540f1c06d7760e6cb3f4b25b70610cb1e0
Amirullah <novel@silverdust.net>
2016-05-16 13:06:43 -0400

    Initial commit

---
+++ Generic/src/main/java/util/DBConnect.java
@@ -0,0 +1,106 @@
+package util;
+
+import base.CommonAPI;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Created by A S M Amirullah on 5/10/2016.
+ */
+class DBConnect {
+    private Connection connect = null;
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+    // List<String> list = new ArrayList<String>();
+
+    public static Properties loadPropertiesFile()throws Exception{
+        Properties prop = new Properties();
+        InputStream ism = new FileInputStream("(path)C:\\Users\\...\\IdeaProjects\\MoonShot\\Generic\\lib\\MySQL.properties");
+        prop.load(ism);
+        ism.close();
+
+        return prop;
+    }
+
+    //Connect to Database
+    public void connectToDataBase() throws Exception{
+        Properties prop = loadPropertiesFile();
+        String driverClass = prop.getProperty("MYSQLJDBC.driver");
+        String url = prop.getProperty("MYSQLJDBC.url");
+        String userName = prop.getProperty("MYSQLJDBC.userName");
+        String passWord = prop.getProperty("MYSQLJDBC.passWord");
+        // This will load the MySQL driver.
+        Class.forName(driverClass);
+        // Setup the connection with the DB
+        connect = DriverManager.getConnection(url, userName, passWord);
+        CommonAPI.logger.info("database connected");
+        // System.out.println("Database connected");
+
+    }
+
+    public List<String> readDataBase(String tableName, String columnName) throws Exception {
+        List<String> data = new ArrayList<String>();
+        try {
+
+            connectToDataBase();
+            // Statements allow to issue SQL queries to the database
+            statement = connect.createStatement();
+            // Result set get the result of the SQL query
+            resultSet = statement
+                    .executeQuery("select * from "+ tableName);
+            data = getResultSetData(resultSet, columnName);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            close();
+        }
+
+        return data;
+    }
+    public void queryDatabase(){
+
+    }
+
+    private List<String> getResultSetData(ResultSet resultSet,String columnName) throws SQLException {
+        List<String> dataList = new ArrayList<String>();
+        while (resultSet.next()) {
+            String itemName = resultSet.getString(columnName);
+
+            dataList.add(itemName);
+
+        }
+
+        return dataList;
+    }
+    private void writeResultSetToConsole(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            String itemName = resultSet.getString("NewsTitle");
+            System.out.println("News Title: " + itemName);
+        }
+
+    }
+
+    private void close() {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (statement != null) {
+                statement.close();
+            }
+
+            if (connect != null) {
+                connect.close();
+            }
+        } catch (Exception e) {
+
+        }
+    }
+}
---
+++ Generic/src/main/java/util/TestDBConnection.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by A S M Amirullah on 5/10/2016.
+ */
+public class TestDBConnection {
+    public static void main(String[] args)throws Exception {
+        DBConnect dbConnect = new DBConnect();
+        // dbConnect.connectToDataBase();
+        //List<String> newsList = dbConnect.readDataBase();
+    }
+}
