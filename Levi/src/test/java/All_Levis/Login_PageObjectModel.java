45f330c4129d3886b5edeb1abf6baef1a45b1204
mimran0 <jmrthan@gmail.com>
2016-05-19 13:50:10 -0400

    + added Levis module2

---
+++ Levi/src/test/java/All_Levis/Login_PageObjectModel.java
@@ -0,0 +1,31 @@
+package All_Levis;
+
+import base.CommonAPI;
+import org.testng.annotations.Test;
+
+/**
+ * Created by imran on 5/4/2016.
+ */
+public class Login_PageObjectModel extends CommonAPI {
+    //HomePage homePage = new HomePage();
+    //HomePage homePage= PageFactory.initElements(driver,HomePage.class);
+   // LoginPage loginPage = new LoginPage();
+    //LoggedInPage loggedInPage = new LoggedInPage();
+
+    @Test
+    public void Loginsuccessfull() throws InterruptedException {
+       // homePage.navigateToLoginPage();
+        //loginPage.Login("limontest9@gmail.com","1234561l");
+        //loggedInPage.LogOut();
+
+
+    }
+//@Test
+//    public static void main(String[] args) throws InterruptedException {
+//    HomePage homePage = new HomePage();
+//    homePage.navigateToLoginPage();
+//    }
+
+
+
+}
